467563daf2a1af20e12da019554f615eea87d8b4
Sebastian Furquet <Sebastianfurquet@gmail.com>
2025-05-16 09:37:57 -0300

    funcionalidad de From contacto

--- src/Components/Contacto.jsx
+++ src/Components/Contacto.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-
+import {useForm} from 'react-hook-form'
 const Contacto = () => {
+
+  const {register, handleSubmit} = useForm();
+
+  const enviar = (data) => {
+    console.log(data)
+  }
+
   return (
     <div className="container">
         <h1 className='main-title'>Contacto</h1>
-        <form className='formulario' >
-            <input type="text" placeholder='Ingresa tu nombre' />
-            <input type="email" placeholder='Ingresa tu E-mail' />
+        <form className='formulario' onSubmit={handleSubmit(enviar)}>
+
+            <input type="text" placeholder='Ingresa tu nombre'    {...register("nombre")}   />
+            <input type="email" placeholder='Ingresa tu E-mail'   {...register("email")}    />
+            <input type="phone" placeholder='Ingresa tu telefono' {...register("telefono")} />
+
             <button className='enviar' type="submit"> Enviar </button>
         </form>
     </div>
